7cc16149750c2cf5c81647973f068e99c557ff21
G00364756 <36041776+G00364756@users.noreply.github.com>
2018-03-23 23:11:40 +0000

    type.py created
    Splits iris data set into 3 classes of iris then fins the maximum sepal length for each of the classes

---
+++ type.py
@@ -0,0 +1,39 @@
+# Aidan O'Connor - G00364756 - 22/03/2018 
+# Programming_Project - Investigate the iris data set and 
+# create analysis based on research previously conducted
+# References:
+# Link to csv file = https://archive.ics.uci.edu/ml/datasets/iris
+# Adapted from code learned from lectures by Dr. Ian McGlouglin.
+
+#This code splits the data set into the 3 different classes of iris and find the maximum sepal length for each class
+Ace=[]
+King=[]
+Queen=[]
+Jack=[]
+
+with open("Data_sets/iris.csv") as f:
+    for line in f:  
+        Ace.append(line.split(","))
+    for x in range(len(Ace)):
+        for i in Ace[x]:
+            if i == "Iris-setosa\n":
+                King.append(Ace[x])
+            elif i =="Iris-versicolor\n":
+                Queen.append(Ace[x])
+            elif i =="Iris-virginica\n":
+                Jack.append(Ace[x])
+            else:
+                continue
+    #end of first part
+    Mickey = []
+    for m in range(len(King)):
+            Mickey.append(King[m][0])
+    print('{}{}{}'.format("The maximum sepal length for the Iris Setosa class is ", max(Mickey), "cm"))
+    Minnie = []
+    for m in range(len(Queen)):
+            Minnie.append(Queen[m][0])
+    print('{}{}{}'.format("The maximum sepal length for the Iris Versicolor class is ", max(Minnie), "cm"))
+    Pluto = []
+    for m in range(len(Jack)):
+            Pluto.append(Jack[m][0])
+    print('{}{}{}'.format("The maximum sepal length for the Iris Virginica class is cm ",max(Pluto), "cm"))
